43963380c00f70a5b524aea071cde180519e64fe
Kyrosta <georgiusm07@gmail.com>
2024-02-19 10:43:28 +0700

    First Push

---
+++ app/src/main/java/com/leon/anmp_week1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.leon.anmp_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leon.anmp_week1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
